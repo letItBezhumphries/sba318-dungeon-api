52301f4acf2e4eb17e97f555b8b5c7dd314ec894
Eric Humphries <ezhumphries@gmail.com>
2024-07-10 22:19:46 -0700

    added possible views to use with pug templates

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 const PORT = process.env.PORT;
-
+const PUBLIC_DIR = path.join(__dirname, 'public');
 const app = express();
 
-// set up middleware
-express.use(express.static('public'));
+/** define Routes */
+const userRoutes = require('./routes/users');
+const characterRoutes = require('./routes/characters');
+const dungeonRoutes = require('./routes/dungeons');
 
-// serve the index.js file
+/** Wiring in route handlers */
+app.use('/users', userRoutes);
+app.use('/characters', characterRoutes);
+app.use('/dungeons', dungeonRoutes);
+
+// set up middleware
+express.use(express.static(PUBLIC_DIR));
+
+// app.use('static', express.static(PUBLIC_DIR));
+
+// serve the index.js file with res.sendFile()
+
+app.set('view engine', 'pug');
 
 app.listen(PORT, () => {
   console.log(`you are listening on localhost on port ${PORT}`);
