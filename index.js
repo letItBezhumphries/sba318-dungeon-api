853007b6073fe9a9e1db7e7201334e0072d2fb87
Eric Humphries <ezhumphries@gmail.com>
2024-07-16 19:08:53 -0700

    sucdessfully set up the user-form to send the values and sucessfully create a user and display that user on the post route render

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const path = require('path');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const PORT = process.env.PORT;
-const PUBLIC_DIR = path.join(__dirname, '/public');
+const PUBLIC_DIR = path.join(__dirname, 'public');
 const app = express();
 
 // const connectDB = require('./db/index');
 /* Pre database */
 const monsters = require('./data/monsters');
 
-console.log('public dir:', PUBLIC_DIR);
 // connectDB();
 
 /** define Routes */
@@ -23,9 +22,6 @@ const monsterRoutes = require('./routes/monsters');
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
-// serve static files
-// app.use('public', express.static(PUBLIC_DIR));
-app.use('static', express.static(PUBLIC_DIR));
 
 /** Wiring in route handlers */
 app.use('/api/users', userRoutes);
@@ -34,18 +30,15 @@ app.use('/api/dungeons', dungeonRoutes);
 app.use('/api/monsters', monsterRoutes);
 // set up middleware
 
-// app.use('static', express.static(PUBLIC_DIR));
-const userNames = ['Sean', 'George', 'Roger', 'Timothy', 'Pierce', 'Daniel'];
-const bestMovie = 'Casino Royale';
+// serve static files
+app.use('public', express.static(PUBLIC_DIR));
 
-// serve the index.js file with res.sendFile()
 // serve up the home page
 app.get('/', (req, res) => {
   res.render('index', {
     page_title: 'Welcome to the D&D Dungeon Builder',
     monstersView: true,
     monsters,
-    userNames,
     authenticated: false,
   });
 });
--- routes/monsters.js
+++ routes/monsters.js
@@ -4,12 +4,15 @@ const router = express.Router();
 /* pre-db monsters data */
 const monsters = require('../data/monsters');
 
-// @route    GET /monsters
+// @route    GET api/monsters
 // @desc     get all monsters
 // @access   Public
 router.get('/', (req, res) => {
   res.status(200).render('monsters', {
+    page_title: 'Monsters available to add to your Dungeons',
+    authenticated: false,
     monsters: monsters,
+    monsterView: monsters[0],
   });
 });
 
